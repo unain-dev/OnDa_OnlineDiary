2c4f5f7d1b69078012b88d9222d380b907b9bdc7
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-04-18 15:58:57 +0900

    S06P31A107-23 Config: 프로젝트 초기 세팅

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  quotes: [2, 'single'],
+  extends: ['airbnb', 'plugin:react/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
