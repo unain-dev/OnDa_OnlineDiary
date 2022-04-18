8dd655563e8c99f1bda656d594cff51406fd5529
jiyoon <jiyoon07125@gmail.com>
2022-04-18 15:50:45 +0900

    #S06P31A107-25 Config: 프로젝트 초기세팅

---
+++ backend/src/main/java/com/ssafy/onda/OndaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.onda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/onda/OndaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.onda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
