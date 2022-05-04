d8e4d3276ba0fbf16f979f56d24031bab1a288be
unain-dev <daeun84366@gmail.com>
2022-05-04 14:20:50 +0900

    #S06P31A107-87 Design: 웹폰트 적용

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,8 @@ import { wrapper } from '../core/store'
 
 import type { AppProps } from 'next/app'
 
+import 'styles/css/globals.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
