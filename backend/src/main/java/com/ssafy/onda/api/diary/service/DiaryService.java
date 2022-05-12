eb0128e5627ca72bcb4955e65f143c0ec8a60d72
김은선 <kus0412@naver.com>
2022-05-12 07:51:13 +0000

    [BE: A5] #S06P31A107-130 이미지 파일 저장 방식 리펙토링

--- backend/src/main/java/com/ssafy/onda/api/diary/service/DiaryService.java
+++ backend/src/main/java/com/ssafy/onda/api/diary/service/DiaryService.java
@@ -6,9 +6,11 @@ import com.ssafy.onda.api.diary.dto.response.ResDiaryDto;
 import com.ssafy.onda.api.diary.entity.Background;
 import com.ssafy.onda.api.member.entity.MemberMemo;
 import com.ssafy.onda.global.common.auth.CustomUserDetails;
+import com.ssafy.onda.global.common.entity.Image;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Set;
 
 public interface DiaryService {
 
@@ -16,7 +18,7 @@ public interface DiaryService {
 
     void deleteByMemberAndDiaryDate(CustomUserDetails details, String diaryDate);
 
-    void delete(Background background);
+    void delete(Background background, Set<Image> archivedImage);
 
     void checkDateValidation(String date);
 
--- backend/src/main/java/com/ssafy/onda/global/common/repository/ImageRepository.java
+++ backend/src/main/java/com/ssafy/onda/global/common/repository/ImageRepository.java
@@ -4,9 +4,12 @@ import com.ssafy.onda.global.common.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     List<Image> findAllByImageSeqIn(List<Long> imageSeqs);
+
+    Optional<Image> findByFileInfoEncodedName(String encodedName);
 
 }
