634c390b0fa23b5169b9fbf09cf91aa766425367
unain-dev <daeun84366@gmail.com>
2022-05-12 17:59:52 +0900

    Fix: cookie domain opotion 추가

--- frontend/pages/user/login.tsx
+++ frontend/pages/user/login.tsx
@@ -1,47 +1,49 @@
-import LoginForm from "component/user/loginForm";
-import { getCookie, removeCookie, setCookie } from "core/common/cookie";
-import { onLogin } from "core/api/memberApi";
-import { useState } from "react";
-import { useRouter } from "next/router";
+import LoginForm from 'component/user/loginForm'
+import { getCookie, removeCookie, setCookie } from 'core/common/cookie'
+import { onLogin } from 'core/api/memberApi'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 const login = () => {
-  const [memberId, setMemberId] = useState("");
-  const [password, setPassword] = useState("");
+  const [memberId, setMemberId] = useState('')
+  const [password, setPassword] = useState('')
 
   const memberIdHandler = (e) => {
-    setMemberId(e.currentTarget.value);
+    setMemberId(e.currentTarget.value)
   }
 
   const passwordHandler = (e) => {
-    setPassword(e.currentTarget.value);
+    setPassword(e.currentTarget.value)
   }
 
   const router = useRouter()
-  
+
   // 로그인 버튼 클릭
   const loginFormSubmit = async () => {
-    const result = await onLogin({memberId, password});
+    const result = await onLogin({ memberId, password })
     if (result.status == 200) {
       // alert(result.msg);
-      setCookie("member", result.data, {
-        path: "/",
+      setCookie('member', result.data, {
+        path: '/',
         secure: true,
-        sameSite: "none",
+        sameSite: 'none',
+        domain: 'http://k6a107.p.ssafy.io/',
       })
       router.push(`/collection/month`)
     } else {
-      alert(result.msg);
+      alert(result.msg)
     }
   }
 
   // 로그아웃 테스트 버튼
   const logout = () => {
     // console.log(getCookie("member"));
-    removeCookie("member", {
-      path: "/",
+    removeCookie('member', {
+      path: '/',
       secure: true,
-      sameSite: "none"
-    });
+      sameSite: 'none',
+      domain: 'http://k6a107.p.ssafy.io/',
+    })
   }
 
   const props = {
@@ -55,9 +57,9 @@ const login = () => {
 
   return (
     <div className="login-page">
-      <LoginForm {...props}/>
+      <LoginForm {...props} />
     </div>
-  );
-};
+  )
+}
 
-export default login;
+export default login
