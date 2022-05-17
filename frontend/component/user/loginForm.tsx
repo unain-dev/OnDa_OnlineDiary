7f9117551dcc8cc767a9e4cf548adbd7254d721a
안지애 <asj8839@naver.com>
2022-05-17 01:51:33 +0000

    [FE: D2] 로그인, 로그아웃 쿠키 설정, 로그인 화면의 엔터키 기능 구형

--- frontend/component/user/loginForm.tsx
+++ frontend/component/user/loginForm.tsx
@@ -9,6 +9,7 @@ const loginForm = ({
   memberIdHandler,
   passwordHandler,
   loginFormSubmit,
+  onkeydown,
   logout,
 }) => {
   
@@ -19,8 +20,8 @@ const loginForm = ({
         <h2>로그인</h2>
       </div>
       <div className={styles.content}>
-        <input type="text" className='member_id' value={memberId} onChange={memberIdHandler} placeholder='아이디를 입력해주세요' required />
-        <input type="password" className='password' value={password} onChange={passwordHandler} placeholder='비밀번호를 입력해주세요' />
+        <input type="text" className='member_id' value={memberId} onChange={memberIdHandler} onKeyDown={onkeydown} placeholder='아이디를 입력해주세요' required />
+        <input type="password" className='password' value={password} onChange={passwordHandler} onKeyDown={onkeydown} placeholder='비밀번호를 입력해주세요' />
         <button className={styles.btn_login} type='button' onClick={loginFormSubmit}>로그인</button>
         <button className={styles.btn_signup} onClick={() => { router.push(`/user/signup`)}}>회원가입</button>
         <button className={styles.btn_login} type='button' onClick={logout}>로그아웃</button>
