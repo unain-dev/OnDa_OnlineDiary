eb861b1d82191ba89b76acf7d9a1fc6e7bad2d37
EJKim3191 <95759697+EJKim3191@users.noreply.github.com>
2022-05-17 18:32:31 +0900

    #S06P31A107-160 Fix: 미리보기 html 테그 제거

--- frontend/component/collection/views/textView.tsx
+++ frontend/component/collection/views/textView.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from '../../../styles/scss/Collection.module.scss'
+import ReactHtmlParser from 'react-html-parser';
 const TextView = ({memoSeq}) =>{
 
     return (
         <div className={styles.textView}>
             <div className={styles.header}>{memoSeq.header}</div>
-            <div className={styles.content}>{memoSeq.content}</div>
+            <div className={styles.content}>{ReactHtmlParser(memoSeq.content)}</div>
         </div>
     )
 } 
