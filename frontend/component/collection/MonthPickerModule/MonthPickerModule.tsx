abe8ba6e9b545a4c4b83037a33df68ca4d5a6063
김응주 <dmdwn3979@naver.com>
2022-05-19 01:01:18 +0000

    [FE: 모아보기 및 HOTFIX] 모아보기 년도 별 월 이동 datepicker 구현 및 이모지 엔터 저장 bugfix

---
+++ frontend/component/collection/MonthPickerModule/MonthPickerModule.tsx
@@ -0,0 +1,34 @@
+import DatePicker from 'react-datepicker'
+import React, { useEffect, useState } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import styles from './MonthPickerModule.module.scss'
+import ko from 'date-fns/locale/ko'
+
+const MonthPickerModule = ({gotoDate}) => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const ExampleCustomInput = ({ value, onClick }) => (
+    <button className={styles.monthBtn} onClick={onClick}>
+      월별 선택
+    </button>
+  );
+
+  return (
+    <>
+      <DatePicker
+        locale={ko}
+        selected={startDate}
+        dateFormat="MM/yyyy"
+        onChange={(date) => {
+          setStartDate(date)
+          gotoDate(date);
+        }}
+        className={styles.datePicker}
+        showMonthYearPicker
+        customInput={<ExampleCustomInput />}
+      />
+    </>
+  )
+}
+
+export default MonthPickerModule
